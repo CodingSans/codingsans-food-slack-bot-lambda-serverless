1de74b4f10f5f0b0c31fb795b95b840a20ca065c
Vincze Dani <vdani@codingsans.com>
2017-09-14 16:13:12 +0200

    revert

--- scripts/serverless.config.js
+++ scripts/serverless.config.js
@@ -1,9 +1,7 @@
-const moment = require('moment');
 const momentTz = require('moment-timezone');
 
 module.exports.schedule = () => {
-  const hour = moment().isDST() ? 12 : 11;
-  const elevenInCet = momentTz.tz('CET').set('hour', hour).utc().get('hours');
+  const elevenInCet = momentTz.tz('CET').set('hour', 11).utc().get('hours');
   return {
     CET11: `cron(0 ${ elevenInCet } ? * MON-FRI *)`,
   };
